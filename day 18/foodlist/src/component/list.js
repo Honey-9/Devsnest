3f06fa4a0f249cc963337e9b0fffa8208b43499d
Honey-9 <harshsingh9667903022@gmail.com>
2021-07-12 21:21:05 +0530

    Completed

---
+++ day 18/foodlist/src/component/list.js
@@ -0,0 +1,15 @@
+const List = ({ data }) => {
+  console.log(data)
+      return (
+          <>
+          { data.map(({ name, cal }, index) => (
+              <div key={ index } className='cards' style={ { color: (index % 2 === 1 ? '#000' : '#999')} }>
+                  <h2>{ name }</h2>
+                  <p>This contains { cal } calories</p>
+              </div>
+          ))}
+          </>
+      );
+}
+
+export default List;
