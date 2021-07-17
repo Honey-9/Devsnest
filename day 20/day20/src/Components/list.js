254d17c90f3ade02fd6f619c3963b9e9c5eb1cab
Honey-9 <harshsingh9667903022@gmail.com>
2021-07-17 22:02:50 +0530

    done

---
+++ day 20/day20/src/Components/list.js
@@ -0,0 +1,20 @@
+import deleteIcon from './delete.svg'
+
+const List = ({ id, item, deleteItem }) => {
+    const { name, cal } = item
+        return (
+            <>
+                <div className='cards'>
+                    <div>
+                        <h2>{ name }</h2>
+                        <p>This contains { cal } calories</p>
+                    </div>
+                    <div>
+                        <img className='del-btn' src={deleteIcon} alt='img' onClick = { (e) => deleteItem(id) }/>
+                    </div>
+                </div>
+            </>
+        );
+}
+ 
+export default List;
