736cb0d68140aea15a6ca86de5336e307db6dd00
Honey-9 <harshsingh9667903022@gmail.com>
2021-07-21 22:14:11 +0530

    done

--- day 21/day/src/App.js
+++ day 21/day/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './components/list';
+import { useState } from 'react';
+import addIcon from './assets/add.svg'
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      name: 'Pizza',
+      calorie: 24
+    },
+    {
+      name: 'Momos',
+      calorie: 35
+    }
+  ])
+
+
+  const [name, setName] = useState('')
+  const [calorie, setCalorie] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setItems([...items, 
+      {
+        name, 
+        calorie,
+      }
+    ])
+    setName('')
+    setCalorie('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='list'>Calorie List</h1>
+      <div className='todo'>
+        <form action="" onSubmit={ handleSubmit }>
+          <label htmlFor="name"> Title </label>
+          <input
+            type='text'
+            placeholder='Type a Food name'
+            name='name'
+            value={ name }
+            onChange={e => setName(e.target.value)}
+            required
+          />
+          <label htmlFor='calorie'> Calorie </label>
+          <input
+            type='number'
+            placeholder='Give Calorie count'
+            name='calorie'
+            value={ calorie }
+            onChange={e => setCalorie(e.target.value)}
+            required
+          />
+          <button type='submit'>
+            <img src={ addIcon } alt="addIcon" />
+          </button>
+        </form>
+      </div>
+      <div className="calorie-list">
+        {items.length ? (
+          items.map((item, index) => (
+            <List
+              item={ item }
+              key={ index }
+              id={ index }
+              items={ items }
+              setItems={ setItems }
+            />
+          ))
+        ) : (
+          <h2 className='empty'>Add Items</h2>
+        )
+        }
+      </div>
     </div>
   );
 }
